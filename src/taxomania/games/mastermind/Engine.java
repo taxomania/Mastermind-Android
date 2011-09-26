3c10a6bacae2144001772933592bc5bced58ce07
Tariq Patel <tariq.patel@gmail.com>
2011-09-26 23:59:20 +0100

    Fixes to Engine class, combinations no longer static

--- src/taxomania/games/mastermind/Engine.java
+++ src/taxomania/games/mastermind/Engine.java
@@ -8,37 +8,39 @@ package taxomania.games.mastermind;
 public class Engine {
     protected static final int TOTAL_NO_PEGS = 6;
     private static final int COMBO_LENGTH = 4;
-    private final static Pair[] sCombination = new Pair[COMBO_LENGTH];
+    private final Pair[] mCombination = new Pair[COMBO_LENGTH];
 
-    public Engine(){
-        for (int i = 0; i < sCombination.length; i++)
-            sCombination[i] = new Pair(Mastermind.sPegs[(int)((TOTAL_NO_PEGS) * Math.random())]);
-    }
+    public Engine() {
+        for (int i = 0; i < mCombination.length; i++)
+            mCombination[i] = new Pair(Mastermind.sPegs[(int) ((TOTAL_NO_PEGS) * Math.random())]);
+    } // Engine()
 
-    public void resetStates(){
-        for (int i=0;i<sCombination.length;i++)
-            sCombination[i].setReady(true);
-    }
+    public void resetStates() {
+        for (int i = 0; i < mCombination.length; i++)
+            mCombination[i].setReady(true);
+    } // resetStates()
 
     // Check the peg is the right colour and in the right position
-    public boolean checkPos(int guess, int pos){
-        if (sCombination[pos].getReady())
-            if (guess == sCombination[pos].getPeg()){
-                sCombination[pos].setReady(false);
+    public boolean checkPos(final int guess, final int pos) {
+        if (mCombination[pos].getReady()) {
+            if (guess == mCombination[pos].getPeg()) {
+                mCombination[pos].setReady(false);
                 return true;
-            }
+            } // if
+        } // if
         return false;
-    } // checkPos
+    } // checkPos(int, int)
 
     // Check the peg is the right colour but in the wrong position
-    public boolean checkPeg(int guess){
-        for (int i=0; i< sCombination.length;i++)
-            if (sCombination[i].getReady())
-                if (guess == sCombination[i].getPeg()){
-                    sCombination[i].setReady(false);
+    public boolean checkPeg(final int guess) {
+        for (int i = 0; i < mCombination.length; i++) {
+            if (mCombination[i].getReady()) {
+                if (guess == mCombination[i].getPeg()) {
+                    mCombination[i].setReady(false);
                     return true;
-                }
-
+                } // if
+            } // if
+        } // for
         return false;
-    } // checkPeg
-}
+    } // checkPeg(int)
+} // Engine
